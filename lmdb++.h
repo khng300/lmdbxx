880f2f68e42cab8d2e3e4e1235c06edfd8559762
Ka Ho Ng <khng300@gmail.com>
2025-02-09 22:14:09 -0500

    add env::get_map, remove env::get_internal_map

--- lmdb++.h
+++ lmdb++.h
@@ -1169,20 +1169,14 @@ public:
   }
 
   /**
-   * @notice WARNING: This is a function to access LMDB's internal memory map, use at your own risk!
+   * @return map address and map size
    */
-  std::string_view get_internal_map() {
+  std::string_view get_map() {
     if (sizeof(int) != 4 || sizeof(long) != 8 || sizeof(void*) != 8) error::raise("get_internal_map: only LP64 supported", 0);
 
-    // This is a hack that depends on the internal layout of LMDB's MDB_env struct. Hopefully there
-    // will be a better way to get me_map at some point.
-
-    char *me_map = *(char**)(((char*)_handle) + 56);
-
-    MDB_envinfo arg;
-    mdb_env_info(_handle, &arg);
-
-    return std::string_view(me_map, arg.me_mapsize);
+    MDB_envinfo2 arg;
+    lmdb::env_info2(_handle, &arg);
+    return std::string_view((char *)arg.me_map, arg.me_mapsize);
   }
 };
 
